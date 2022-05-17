cbeba4d7f81f46a129ce75c0c8786cd800b63aad
chan1992241 <chan1992241@gmail.com>
2022-05-17 23:59:42 +0800

    Handle Delete Budget

--- client/src/components/ViewExpensesModal.tsx
+++ client/src/components/ViewExpensesModal.tsx
@@ -1,6 +1,7 @@
 import { Modal, Stack, Button } from "react-bootstrap";
 import { currencyFormatter } from "../utils/currencyFormatter";
 import { FC } from "react";
+import axios from "axios";
 
 interface ViewExpensesModalProps {
     budgetId: number | null,
@@ -11,6 +12,20 @@ export const ViewExpensesModal: FC<ViewExpensesModalProps> = ({ budgetId, handle
     // const { getBudgetExpenses, budgets, deleteBudget, deleteExpense } = useBudgets()
     // const budget = UNCATEGORIZED_BUDGET_ID === budgetId ? { name: "Uncategorized", id: UNCATEGORIZED_BUDGET_ID } : budgets.find(budget => budget.id === budgetId)
     // const expenses = getBudgetExpenses(budgetId)
+    async function handleDeleteBudget(budgetId: number) {
+        try {
+            const response = await axios.delete(`http://localhost:5000/deleteBudget/${budgetId}`)
+            if (response.status === 200) {
+                //redirect to home page
+                if (typeof window !== 'undefined') {
+                    window.location.href = window.location.origin;
+                }
+
+            }
+        } catch (err) {
+            console.error(err)
+        }
+    }
     return (
         <Modal show={budgetId !== null} onHide={handleClose}>
             <Modal.Header closeButton>
@@ -19,7 +34,7 @@ export const ViewExpensesModal: FC<ViewExpensesModalProps> = ({ budgetId, handle
                         <div>
                             Expense - Expense Name
                         </div>
-                        <Button variant="outline-danger" onClick={() => { handleClose() }}>Delete Budget</Button>
+                        <Button variant="outline-danger" onClick={() => budgetId && handleDeleteBudget(budgetId)}>Delete Budget</Button>
                     </Stack>
                 </Modal.Title>
             </Modal.Header>
